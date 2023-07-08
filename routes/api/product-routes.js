9b4ecc4b37b54736010ca415282b7c611cbb0582
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-07-08 16:11:18 -0500

    added a new route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -33,8 +33,13 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
+  Product.create({
+    product_name: Basketball,
+  price:200.00,
+  stock:3,
+  tagIds[1,2,3,4,]
+  })
+    .then((newProduct) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -46,7 +51,7 @@ router.post('/', (req, res) => {
         return ProductTag.bulkCreate(productTagIdArr);
       }
       // if no product tags, just respond
-      res.status(200).json(product);
+      res.status(200).json(newProduct);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {

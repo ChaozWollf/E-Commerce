b855fa95294f65ad3a6605ce23118788c3cb127c
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-07-08 22:32:01 -0500

    working on getting it to run

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const routes = require('./routes');
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+  // Database name
+  DB_NAME='ecommerce_db',
+  // User
+  DB_USER ='root',
+  // Password
+  DB_PASSWORD='MySql',
+  {
+    // Database location
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
+
 // import sequelize connection
 
 const app = express();
@@ -10,7 +27,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+
+await sequelize.sync({ force: true });
+console.log("All models were synchronized successfully.");
+
+
 // sync sequelize models to the database, then turn on the server
+
+
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
